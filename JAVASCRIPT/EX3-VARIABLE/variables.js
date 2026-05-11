bf373fe805c6cf2c8aa55fc759e7d755d5ea494b
Steven <bailleux.steven@gmail.com>
2026-05-11 14:39:29 +0200

    DOCS : Création fichiers exercice 3 (variable) Javascript

---
+++ JAVASCRIPT/EX3-VARIABLE/variables.js
@@ -0,0 +1,15 @@
+
+let nom = "BAILLEUX";
+let prenom = "Steven"
+let age = 30;
+let estMajeur = true;
+let tableau =[1,2,3,4,5];
+
+
+
+
+console.log(nom);
+console.log(prenom);
+console.log(age);
+console.log(estMajeur);
+console.log(tableau);
