c6e9fe18b42335299124882f4e20f274539bf3f4
Steven <bailleux.steven@gmail.com>
2026-05-11 16:31:24 +0200

    DOCS : Création fichiers exercice 4 (compteur) Javascript

---
+++ JAVASCRIPT/EX4-COMPTEUR/compteur.js
@@ -0,0 +1,37 @@
+let addition = 0;
+addition ++;
+
+let soustraction = 0
+soustraction --;
+
+let multiplication = 100
+multiplication *=2;
+
+let division = 100
+division /=2;
+
+let nombreVirgule = 50.25;
+nombreVirgule +=9.75;
+
+let superCalcul = division + nombreVirgule;
+
+let superMegaCalcul = division + nombreVirgule - soustraction + addition * multiplication;
+
+let puissance = 2 ** 10;
+
+let superPuissance = 3 ** 500
+
+console.log(addition);
+console.log(soustraction);
+console.log(multiplication);
+console.log(division);
+console.log(nombreVirgule);
+console.log(superCalcul);
+console.log(superMegaCalcul);
+console.log(puissance);
+console.log(superPuissance);
+
+
+// ou 
+
+console.log(addition, soustraction, multiplication, division, nombreVirgule, superCalcul, superMegaCalcul, puissance, superPuissance);
