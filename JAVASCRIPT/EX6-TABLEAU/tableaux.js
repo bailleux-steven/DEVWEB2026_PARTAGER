9b0169e1fb50008b76c6d519d3327e9f439d75b8
Steven <bailleux.steven@gmail.com>
2026-05-12 12:24:51 +0200

    DOCS : Création fichiers exercice 6 (tableaux) Javascript

---
+++ JAVASCRIPT/EX6-TABLEAU/tableaux.js
@@ -0,0 +1,10 @@
+let nom = "Ronflex";
+let age = 100;
+let passions = ["Manger","Dormir"];
+let tabUser = [(nom),(age),(passions)];
+
+console.log (tabUser);
+
+console.log (tabUser[2]);
+
+console.log (tabUser[2][1]);
